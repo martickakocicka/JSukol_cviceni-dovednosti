28c1f8085f315e1a0a0014e5c3ce1945792cac66
Marta Bakešová <marta.bakesova@centrum.cz>
2024-03-06 12:45:57 +0100

    nevim

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-console.log('funguju!');
 const urovenH = prompt("zadej svou úroveň v HTML 0-100")
+console.log("uroven html: " +urovenH)
 const urovenC = prompt("zadej svou úroveň v CSS 0-100")
+console.log("uroven css: " +urovenC)
 const urovenJ = prompt("zadej svou úroveň v JavaScript 0-100")
+console.log("uroven JS: "+urovenJ)
+
+const 
 
 
